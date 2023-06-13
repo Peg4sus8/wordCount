28c7b15ab49fafc2b3fa06706f41406fdda224bf
Peg4sus8 <v.malafronte7@studenti.unisa.it>
2023-06-13 14:15:15 +0000

    final upload

--- benchmark.sh
+++ benchmark.sh
@@ -2,53 +2,53 @@
 make
 export OMPI_MCA_btl_vader_single_copy_mechanism=none
 
-mpirun --allow-run-as-root -np 1 ./main
-mpirun --allow-run-as-root -np 1 ./main
-mpirun --allow-run-as-root -np 1 ./main
-mpirun --allow-run-as-root -np 2 ./main
-mpirun --allow-run-as-root -np 2 ./main
-mpirun --allow-run-as-root -np 2 ./main
-mpirun --allow-run-as-root -np 3 ./main 
-mpirun --allow-run-as-root -np 3 ./main 
-mpirun --allow-run-as-root -np 3 ./main 
-mpirun --allow-run-as-root -np 4 ./main
-mpirun --allow-run-as-root -np 4 ./main
-mpirun --allow-run-as-root -np 4 ./main
-mpirun --allow-run-as-root -np 5 ./main 
-mpirun --allow-run-as-root -np 5 ./main 
-mpirun --allow-run-as-root -np 5 ./main 
-mpirun --allow-run-as-root -np 6 ./main 
-mpirun --allow-run-as-root -np 6 ./main 
-mpirun --allow-run-as-root -np 6 ./main 
-mpirun --allow-run-as-root -np 7 ./main 
-mpirun --allow-run-as-root -np 7 ./main 
-mpirun --allow-run-as-root -np 7 ./main 
-mpirun --allow-run-as-root -np 8 ./main 
-mpirun --allow-run-as-root -np 8 ./main 
-mpirun --allow-run-as-root -np 8 ./main 
-mpirun --allow-run-as-root -np 9 ./main 
-mpirun --allow-run-as-root -np 9 ./main 
-mpirun --allow-run-as-root -np 9 ./main 
-mpirun --allow-run-as-root -np 10 ./main 
-mpirun --allow-run-as-root -np 10 ./main 
-mpirun --allow-run-as-root -np 10 ./main 
-mpirun --allow-run-as-root -np 11 ./main 
-mpirun --allow-run-as-root -np 11 ./main 
-mpirun --allow-run-as-root -np 11 ./main 
-mpirun --allow-run-as-root -np 12 ./main 
-mpirun --allow-run-as-root -np 12 ./main 
-mpirun --allow-run-as-root -np 12 ./main 
-mpirun --allow-run-as-root -np 13 ./main 
-mpirun --allow-run-as-root -np 13 ./main 
-mpirun --allow-run-as-root -np 13 ./main 
-mpirun --allow-run-as-root -np 14 ./main 
-mpirun --allow-run-as-root -np 14 ./main 
-mpirun --allow-run-as-root -np 14 ./main 
-mpirun --allow-run-as-root -np 15 ./main 
-mpirun --allow-run-as-root -np 15 ./main 
-mpirun --allow-run-as-root -np 15 ./main 
-mpirun --allow-run-as-root -np 16 ./main 
-mpirun --allow-run-as-root -np 16 ./main 
-mpirun --allow-run-as-root -np 16 ./main
+mpirun --allow-run-as-root -np 1 --hostfile hfile ./main
+mpirun --allow-run-as-root -np 1 --hostfile hfile ./main
+mpirun --allow-run-as-root -np 1 --hostfile hfile ./main
+mpirun --allow-run-as-root -np 2 --hostfile hfile ./main
+mpirun --allow-run-as-root -np 2 --hostfile hfile ./main
+mpirun --allow-run-as-root -np 2 --hostfile hfile ./main
+mpirun --allow-run-as-root -np 3 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 3 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 3 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 4 --hostfile hfile ./main
+mpirun --allow-run-as-root -np 4 --hostfile hfile ./main
+mpirun --allow-run-as-root -np 4 --hostfile hfile ./main
+mpirun --allow-run-as-root -np 5 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 5 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 5 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 6 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 6 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 6 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 7 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 7 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 7 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 8 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 8 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 8 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 9 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 9 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 9 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 10 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 10 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 10 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 11 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 11 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 11 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 12 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 12 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 12 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 13 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 13 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 13 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 14 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 14 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 14 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 15 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 15 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 15 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 16 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 16 --hostfile hfile ./main 
+mpirun --allow-run-as-root -np 16 --hostfile hfile ./main
 
 make clean
